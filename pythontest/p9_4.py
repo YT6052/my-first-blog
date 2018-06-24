3051a84625009125c191ce388a0370f6bd4f84bd
YT6052 <a5816052@aoyama.jp>
2018-06-24 19:30:09 +0900

    My Django Girls app, first commit

--- pythontest/p9_4.py
+++ pythontest/p9_4.py
@@ -12,12 +12,14 @@ np.random.seed(19680801)
 
 
 fig, ax = plt.subplots()
-ax.plot(np.random.rand(20), '-o', ms=20, lw=2, alpha=0.7, mfc='orange')
+
+ax.plot(np.arange(1, 13, 1), np.random.uniform(30, 80, 12), '-o', ms=5, lw=1, alpha=0.7, mfc='blue')
 ax.grid()
 
-# position bottom right
-fig.text(0.95, 0.05, 'Property of MPL',
-         fontsize=50, color='gray',
-         ha='right', va='bottom', alpha=0.5)
-
+plt.xlim(1, 12)
+plt.ylim(30, 80)
+plt.xlabel('Month')
+plt.ylabel('Money [10000yen]')
+plt.title('Earned money per month in 2017')
+plt.minorticks_on()
 plt.show()
