99577304594be1a692397f0718860028f40d25c2
YT6052 <a5816052@aoyama.jp>
2018-07-02 15:13:28 +0900

    Changed the HTML for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
 ]
+=======
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+]
+>>>>>>> 50ff0d4f3d79463df05e4b2870cd3af10f585c79
